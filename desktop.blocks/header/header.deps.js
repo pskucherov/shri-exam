8a5103fd77663580836a66c939de6cba30cc3fd7
Pavel <php_prog@mail.ru>
2013-10-04 12:35:39 +0400

    View of the header is ready

--- desktop.blocks/header/header.deps.js
+++ desktop.blocks/header/header.deps.js
@@ -3,6 +3,7 @@
         { block: 'b-link' },
         { block: 'b-logo' },
         { block: 'arrow' },
+        { block: 'b-menu-horiz', mods: { layout: 'normal' } },
         { elem: 'line' }
     ]
 })

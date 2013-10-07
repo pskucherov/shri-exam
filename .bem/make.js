c38e7aee0538659c7bd88a569b0cef95ed183da2
root <root@debian.(none)>
2013-10-07 12:03:33 +0300

    bem-node-test init + (folders desktop.blocks, desktop.bundles)

--- .bem/make.js
+++ .bem/make.js
@@ -1,41 +1,28 @@
-/* jshint node:true */
-/* global MAKE */
-
-require('bem-environ/lib/nodes');
-
-//process.env.YENV = 'production';
+var cwd = process.cwd();
 
 MAKE.decl('Arch', {
 
-    blocksLevelsRegexp: /^.+?\.blocks/,
-    bundlesLevelsRegexp: /^.+?\.bundles$/,
-
-    libraries: [
-        'bem-bl @ 0.3',
-        'bem-controls @ v1'
-    ]
+    getBundlesLevels: function() {
+        return [
+            cwd + '/pages'
+        ];
+    }
 
 });
-
 
 MAKE.decl('BundleNode', {
 
     getTechs: function() {
 
         return [
-            'bemjson.js',
             'bemdecl.js',
             'deps.js',
             'bemhtml',
-            'js',
             'css',
-            'ie.css',
-            'ie7.css',
-            'ie8.css',
-            'ie9.css',
-            'html'
+            'js',
+            'priv.js',
+            'server.js'
         ];
-
     }
 
 });
--- desktop.bundles/graduates/graduates.bemjson.js
+++ desktop.bundles/graduates/graduates.bemjson.js
@@ -1,5 +1,10 @@
 ({
     block: 'b-page',
+    title: 'Страница выпускников',
+    head: [
+        { elem: 'css', url: '_graduates.css', ie: false },
+        { elem: 'css', url: '_graduates', ie: true },
+    ],
     content: {
         block: 'graduates',
         content: "Страница выпускников"

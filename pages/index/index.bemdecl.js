643e3862074830cb9885b07b04d4535ee7d984da
phpprog <php_prog@mail.ru>
2013-10-11 12:10:23 +0300

    Fixed a bug in gitinore because of which was not included file *.bemdecl.js. (+ min refact in header.bemhtml)

---
+++ pages/index/index.bemdecl.js
@@ -0,0 +1,15 @@
+exports.blocks = [
+
+    // bem-node part
+
+    {block: 'i-router', mods: {init: 'auto'}},
+    {block: 'i-content', mods: {type: 'bemhtml'}},
+    {block: 'i-ycssjs'},
+
+    // user part
+    {block: 'i-page-home'},
+    { block: 'i-page-lectures' },
+    { block: 'i-page-lecture' }
+
+
+]

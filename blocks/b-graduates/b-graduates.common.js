62ceba9e587f16cbc8ab3f55b44a7376ca81597a
phpprog <php_prog@mail.ru>
2013-10-13 02:35:55 +0300

    page-list set in __page-list, create __page-faces

--- blocks/b-graduates/b-graduates.common.js
+++ blocks/b-graduates/b-graduates.common.js
@@ -2,69 +2,14 @@ BEM.JSON.decl({name: 'b-graduates'}, {
 
     onBlock: function(ctx) {
 
+        var faceFlag = ctx.param('faces');
+
         ctx.defer(
 
             BEM.blocks['i-api-index'].graduates()
                 .then(function (result) {
-                    var i
-                        , content = []
-                        , graduate;
 
                     BEM.blocks['i-page'].setTitle('Выпускники');
-
-
-
-                    for ( i in result ) {
-
-                        graduate = result[i];
-
-                        content.push({
-                            block: 'b-wrapper-content',
-                            elem: 'divlink',
-                            content: {
-
-                                block: 'b-wrapper-content',
-                                mix: [ { block: 'b-lectures', elem: 'curshover' } ],
-                                content: [
-                                    {
-                                        elem: 'title',
-                                        content: [
-                                            {
-                                                block: 'b-graduates',
-                                                elem: 'title',
-                                                content: {
-                                                    block: 'b-link',
-                                                    url: '/graduates/' + i,
-                                                    content: graduate.name + ', г. ' + graduate.city
-                                                }
-                                            },
-                                            {
-                                                block: 'b-social',
-                                                elem: 'style',
-                                                items: {
-                                                    "link_facebook": graduate.link_facebook,
-                                                       "link_gihub": graduate.link_gihub,
-                                                        "link_yaru": graduate.link_yaru,
-                                                          "link_vk": graduate.link_vk
-                                                }
-                                            },
-                                        ]
-                                    },
-                                    {
-                                        block: 'b-graduates',
-                                        elem: 'avatara',
-                                        photo: 'small_' + graduate['link_photo']
-                                    },
-                                    {
-                                        block: 'b-graduates',
-                                        elem: 'shortabout',
-                                        url: '/graduates/' + i,
-                                        content: graduate.about
-                                    }
-                                ]
-                            }
-                        });
-                    }
 
                     ctx.content([
                         /*{
@@ -76,7 +21,9 @@ BEM.JSON.decl({name: 'b-graduates'}, {
                             content: 'Выпускники'
                         },
                         {
-                            content: content
+                            block: 'b-graduates',
+                            elem: (faceFlag) ? 'page-faces' : 'page-list',
+                            result: result
                         }
 
                     ]);
--- blocks/b-graduates/b-graduates.deps.js
+++ blocks/b-graduates/b-graduates.deps.js
@@ -3,6 +3,8 @@
         { block: 'b-link' },
         { block: 'i-api-index' },
         { block: 'b-social' },
-        { elems: [ 'avatara', 'shortabout', 'title', 'readmore' ] }
+        { elems: [ 'avatara', 'shortabout', 'title', 'readmore', 'page-list', 'page-faces' ] },
+        { mods: { posit: 'faces' } },
+        { block: 'b-wrapper-content', mods: { faces: 'yes' } }
     ]
 })
